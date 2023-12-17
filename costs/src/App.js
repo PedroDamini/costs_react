110a6a40fc7693666679a71945795af336ab1e71
unknown <pedrodelemosdamini@gmail.com>
2023-12-17 17:19:14 -0300

    importando o roteamento do DOM

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,9 +1,15 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <li>
+          Home
+        </li>
+      </ul>
+    </Router>
   );
 }
 
