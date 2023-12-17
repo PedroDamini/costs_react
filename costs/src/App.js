e4ae5d6e679e35dbe26c2fd569b1d11e2e048d83
unknown <pedrodelemosdamini@gmail.com>
2023-12-17 18:01:19 -0300

    criando as primeiras rotas

--- costs/src/App.js
+++ costs/src/App.js
@@ -5,10 +5,28 @@ function App() {
   return (
     <Router>
       <ul>
-        <li>
-          Home
-        </li>
+        <li> Home </li>
+        <li> Contato </li>
       </ul>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+
+        <Route exact path='/company'>
+          <Company/>
+        </Route>
+
+        <Route exact path='/contact'>
+          <Contact />
+        </Route>
+
+        <Route exact path='/newproject'>
+          <NewProject />
+        </Route>
+
+      </Switch>
+      <p>Footer</p>
     </Router>
   );
 }
