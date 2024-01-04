95510af012552579f6fc67606bfb7c5017bea3da
unknown <pedrodelemosdamini@gmail.com>
2024-01-04 16:06:40 -0300

    criando uma simulacao de BD db.json

--- costs/src/components/project/ProjectForm.jsx
+++ costs/src/components/project/ProjectForm.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react';
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SumbitButton from '../form/SubmitButton';
 import styles from './ProjectForm.module.css';
 
 function ProjectForm({btnText}) {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'Content-type': "application/json",
+        }
+    })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    }, []);
+    
     return (
         <form className={styles.form}>
             <Input 
@@ -21,6 +38,7 @@ function ProjectForm({btnText}) {
             <Select 
                 name="category_id" 
                 text='Selecione a categoria'
+                options={categories}
             />
             <SumbitButton text={btnText}/>
         </form>
