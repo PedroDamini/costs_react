1ed739ee2cc9e7ed8efd9968e316852f12c7deef
unknown <pedrodelemosdamini@gmail.com>
2024-01-04 21:28:44 -0300

    criando o post do projeto

--- costs/src/components/form/Select.jsx
+++ costs/src/components/form/Select.jsx
@@ -7,7 +7,10 @@ function Select({ text, name, options, handleOnChange, value }) {
             <label htmlFor={name}>{text}:</label>
             <select 
                 name={name} 
-                id={name}>
+                id={name}
+                onChange={handleOnChange}
+                value={value || ''}
+                >
                 <option>Selecione uma opção</option>
                 {options.map((option) => (
                     <option value={option.id} key={option.id}>{option.name}</option>
