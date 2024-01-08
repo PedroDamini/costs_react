089806d168115cdb41a7518fc31b0fdd458c4317
unknown <pedrodelemosdamini@gmail.com>
2024-01-08 19:04:36 -0300

    criando as mensagens de retorno

--- costs/src/components/pages/Projects.jsx
+++ costs/src/components/pages/Projects.jsx
@@ -1,11 +1,19 @@
+import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
 
 
 function Projects() {
+
+    const  location = useLocation();
+    let message = '';
+    if(location.state) {
+        message = location.state.message
+    }
+
     return (
         <div>
             <h1> Projects </h1>
-            <Message msg="Alguma mensagem"/>
+            <Message msg="Alguma mensagem" />
         </div>
     )
 }
