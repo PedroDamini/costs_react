40c6d2d03b17a8b1913aa7c9d741ded3f65434ac
unknown <pedrodelemosdamini@gmail.com>
2024-01-14 23:36:07 -0300

    ajuste project card

--- costs/src/components/project/ProjectCard.jsx
+++ costs/src/components/project/ProjectCard.jsx
@@ -4,13 +4,13 @@ import { BsPencil, BsFillTrashFill } from 'react-icons/bs';
 
 function ProjectCard( {id, name, budget, category, handleRemove} ) {
     return (
-        <div>
+        <div className={styles.project_card}>
             <h4>{name}</h4>
             <p>
                 <span>Orçamento: </span> R${budget}
             </p>
-            <p>
-                <span></span> category
+            <p className={styles.category_text}>
+                <span className={`${styles[category.name.toLowerCase()]}`}></span> category
             </p>
             <div>
                 <p>Editar</p>
