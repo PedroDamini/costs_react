2421dab5702147e0c884644b3428ae1fd72b1043
PedroDamini <pedrodelemosdamini@gmail.com>
2024-01-25 15:17:25 -0300

    criando area de servicos

--- costs/src/components/pages/Project.jsx
+++ costs/src/components/pages/Project.jsx
@@ -11,6 +11,7 @@ function Project () {
     const { id } = useParams();
     const [project, setProject] = useState([]);
     const [showProjectForm, setShowProjectForm] = useState(false);
+    const [showServiceForm, setShowServiceForm] = useState(false);
     const [message, setMessage] = useState();
     const [type, setType] = useState();
 
@@ -58,6 +59,10 @@ function Project () {
         setShowProjectForm(!showProjectForm)
     }
 
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
+    }
+
     return (<>
         {project.name ? (
             <div className={styles.project_details}>
@@ -90,6 +95,23 @@ function Project () {
                             </div>
                         )}
                     </div>
+                    <div className={styles.service_form_container}>
+                        <button className={styles.btn} onClick={toggleServiceForm}>
+                                {!showServiceForm ? 'Adicionar serviço' : 'Fechar'} 
+                        </button>
+                        <div className={styles.project_info}>
+                            {showServiceForm && (
+                                <div>
+                                    formulario de serviço
+                                </div>
+                                )
+                            }
+                        </div>
+                    </div>
+                    <h2>Serviços</h2>
+                    <Container customClass="start">
+                        <p>Itens de Serviços</p>
+                    </Container>
                 </Container>
             </div>
             ) : ( <Loading/>
